1e47c79373be67bf7f84b25419b6ffee3e90b6e9
ToDou <yytodou123@gmail.com>
2015-09-20 09:45:50 +0800

    add the edit

--- multitagview/src/main/java/com/loopeer/android/librarys/multitagview/MultiTagView.java
+++ multitagview/src/main/java/com/loopeer/android/librarys/multitagview/MultiTagView.java
@@ -1,17 +1,13 @@
 package com.loopeer.android.librarys.multitagview;
 
 import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
-import android.text.Editable;
-import android.text.InputFilter;
 import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -23,16 +19,13 @@ import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
 
 public class MultiTagView extends LinearLayout {
 
     private final static String TAG = "MultiTagView";
+    private final static String CURSOR_DRAWABLE_NAME = "mCursorDrawableRes";
 
     public interface TagChangeListener {
         void onTagClick(String tag);
@@ -52,6 +45,12 @@ public class MultiTagView extends LinearLayout {
     private boolean showAddButton;
     private int tagMargin;
     private int tagPadding;
+    private ColorStateList textColorHint;
+    private ColorStateList tagTextColor;
+    private ColorStateList editTextColor;
+    private int cursorDrawableId;
+    private int tagBackgroundId;
+    private float textSize;
 
     private TagChangeListener mTagChangeListener;
 
@@ -75,6 +74,17 @@ public class MultiTagView extends LinearLayout {
         showAddButton = a.getBoolean(R.styleable.MultiTagView_showEditButton, false);
         tagMargin = a.getDimensionPixelSize(R.styleable.MultiTagView_tagMargin, DEFAULT_TAG_MARGIN);
         tagPadding = a.getDimensionPixelSize(R.styleable.MultiTagView_tagPadding, DEFAULT_TAG_PADDING);
+        textColorHint = a.getColorStateList(R.styleable.MultiTagView_textColorHint);
+        tagTextColor = a.getColorStateList(R.styleable.MultiTagView_tagTextColor);
+        tagTextColor = a.getColorStateList(R.styleable.MultiTagView_tagTextColor);
+        editTextColor = a.getColorStateList(R.styleable.MultiTagView_tagEditTextColor);
+        cursorDrawableId = a.getResourceId(R.styleable.MultiTagView_cursorDrawable, R.drawable.cusor_edit);
+        tagBackgroundId = a.getResourceId(R.styleable.MultiTagView_tagBackground, R.drawable.selector_tag_bg);
+        textSize = a.getDimensionPixelSize(R.styleable.MultiTagView_textSize, 0);
+
+        tagTextColor = tagTextColor == null ? ContextCompat.getColorStateList(getContext(), R.color.selector_tag_text) : tagTextColor;
+        editTextColor = editTextColor == null ? ContextCompat.getColorStateList(getContext(), R.color.tag_edit_text_color) : editTextColor;
+        textColorHint = textColorHint == null ? ContextCompat.getColorStateList(getContext(), R.color.tag_text_color_hint) : textColorHint;
         setUpTreeObserver();
         init();
     }
@@ -134,17 +144,18 @@ public class MultiTagView extends LinearLayout {
         editText.setMinimumWidth(2);
         editText.setImeOptions(EditorInfo.IME_ACTION_DONE);
         editText.setSingleLine();
-        /*try {
-            Field f = TextView.class.getDeclaredField("mCursorDrawableRes");
+        try {
+            Field f = TextView.class.getDeclaredField(CURSOR_DRAWABLE_NAME);
             f.setAccessible(true);
-            f.set(editText, R.drawable.cusor_edit);
+            f.set(editText, cursorDrawableId);
         } catch (Exception ignored) {
-        }*/
-        /*
+        }
+
+        editText.setBackgroundResource(tagBackgroundId);
         editText.setPadding(0, dip2px(DEFAULT_TAG_PADDING_TOP), 0, dip2px(DEFAULT_TAG_PADDING_TOP));
         editText.setHint(getResources().getString(R.string.tag_add));
-        editText.setHintTextColor(getResources().getColor(R.color.text_color_hint));
-        editText.setTextColor(getResources().getColor(R.color.text_color_primary));
+        editText.setHintTextColor(textColorHint);
+        editText.setTextColor(editTextColor);
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
@@ -153,62 +164,52 @@ public class MultiTagView extends LinearLayout {
                         return false;
                     }
                     insertTag(editText.getText().toString().trim());
-                    editString = null;
-                    mTagChangeListener.onTagEdited();
                     refresh();
                     return true;
                 }
                 return false;
             }
         });
-        editText.setBackgroundResource(android.R.color.white);
-        editText.setTextSize(14);
+        if (textSize != 0) {
+            editText.setTextSize(textSize);
+        }
         int textEditTextWidth = (int) editText.getPaint().measureText(getResources().getString(R.string.tag_add));
         LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, dip2px(DEFAULT_TAG_HEIGHT));
-
-        tempWidth += dip2px(DEFAULT_TAG_MARGIN) + textEditTextWidth; //add tag width
-
-        if (tempWidth - dip2px(DEFAULT_TAG_MARGIN) > getWidth()) {  //if out of screen, add a new layout
-            mLayoutItem = new LinearLayout(mContext);
-            LayoutParams lParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-            lParams.topMargin = dip2px(DEFAULT_LAYOUT_MARGIN_TOP);
-            mLayoutItem.setLayoutParams(lParams);
-            addView(mLayoutItem);
-            tempWidth = dip2px(DEFAULT_TAG_MARGIN) + mEditTextWidth;
+        tempWidth += textEditTextWidth;
+        if (tempWidth > getWidth()) {
+            addNewLayoutItemWithTopMargin();
+            tempWidth = tagMargin + textEditTextWidth;
+        } else {
+            tempWidth += tagMargin;
         }
+        layoutParams.rightMargin = tagMargin;
         mLayoutItem.addView(editText, layoutParams);
         editText.requestFocus();
-        tempWidth -= dip2px(DEFAULT_TAG_MARGIN) + mEditTextWidth;
-        editText.setOnFocusChangeListener(new OnFocusChangeListener() {
+        tempWidth -= tagMargin + textEditTextWidth;
+        editText.postDelayed(new Runnable() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    mOnCheckDeleteViewListener.checkDeleteView();
-                }
+            public void run() {
+                InputMethodManager imm = (InputMethodManager) editText.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(0, InputMethodManager.SHOW_FORCED);
             }
-        });
-        editText.setFocusable(true);
-        editText.setFocusableInTouchMode(true);
-        editText.requestFocus();
-        if (isFirstIn && TextUtils.isEmpty(editString) && showAddButton) {
-            isFirstIn = false;
-            editText.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    InputMethodManager imm = (InputMethodManager) editText.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.toggleSoftInput(0, InputMethodManager.SHOW_FORCED);
-                }
-            }, 300);
-        }*/
+        }, 300);
+
+    }
+
+
+    private void insertTag(String s) {
+        tags.add(s);
     }
 
     private void addTag(final String tag) {
         final Button button = new Button(mContext);
         button.setText(tag);
 
-        button.setTextSize(14);
-        button.setBackgroundResource(R.drawable.selector_tag_bg);
-        button.setTextColor(ContextCompat.getColorStateList(getContext(), R.color.selector_tag_text));
+        if (textSize != 0) {
+            button.setTextSize(textSize);
+        }
+        button.setBackgroundResource(tagBackgroundId);
+        button.setTextColor(tagTextColor);
         button.setPadding(tagPadding, dip2px(DEFAULT_TAG_PADDING_TOP),
                 tagPadding, dip2px(DEFAULT_TAG_PADDING_TOP));
         button.setEnabled(tagClickable);
